54ead01cbff31959b3ee62540159d7eb66901073
Abner Qian <abner@playhouse.so>
2023-07-25 22:03:24 -0700

    Remove old foot pic when new one is loading

--- src/components/HeelPortrait.tsx
+++ src/components/HeelPortrait.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export interface HeelPortraitProps {
   footId: number;
 }
 
 export function HeelPortrait({footId}: HeelPortraitProps) {
+  const [refresh, setRefresh] = useState(false)
+
+  useEffect(() => {
+    setRefresh(true)
+  }, [footId])
+
+  useEffect(() => {
+    if (refresh) {
+      setRefresh(false)
+    }
+  }, [refresh])
+
+  if (refresh) {
+    return <div className="w-[208px] h=[232px]"/>
+  }
+
   return (
     <Image
       src={`/feet/${footId}.png`}
