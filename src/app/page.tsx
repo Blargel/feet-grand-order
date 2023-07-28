c80a055aceb6119a1ed97b96ab54e264de861c57
Abner Qian <abner@playhouse.so>
2023-07-28 04:20:01 -0700

    Add google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import Script from 'next/script'
 import { Game } from "./Game";
 
 export default function Home() {
-  return <Game />;
+  return (
+    <div>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-Z4PQKX0FBQ" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Z4PQKX0FBQ');
+        `}
+      </Script>
+      <Game />
+    </div>
+  );
 }
